5584a68d52ca6978310e58631f9b8c9a7f5a744d
vurmux <vurmux@gmail.com>
2016-10-06 20:20:21 +0300

    Overseer refactoring

--- gorynych/overseer.py
+++ gorynych/overseer.py
@@ -4,8 +4,8 @@
 import os
 import sys
 import json
-import imp
-import inspect
+from imp import load_module, find_module
+from inspect import getmembers, isclass
 import time
 import codecs
 from pkg_resources import resource_string
@@ -18,56 +18,55 @@ class Overseer(object):
         raw_config = raw_config.decode('utf-8').replace('<HOME>', os.environ['HOME'])
         config = json.loads(raw_config)
         self.config = config
-        self.scarabs = {}
+        self._scarabs = {}
 
+    @property
+    def scarabs(self):
+        return self._scarabs
+
+    def get_scarab(scarab_name):
+        if scarab_name in self._scarabs:
+            return self._scarabs[scarab_name]
+        return None
+
+    def list_scarabs(self):
+        return list(self._scarabs.keys())
+    
     def load_config(self, config):
         self.config = config
 
     def load_folder(self, folder):
         for (dirpath, dirnames, filenames) in os.walk(folder):
             for filename in filenames:
+                name, filetype = (
+                    filename.rsplit('.', 1)
+                    if filename.find('.') != -1
+                    else (filename, None)
+                )
                 if filename == '__init__.py':
                     continue
-                full_path = dirpath + filename
-                if filename.find('.') != -1:
-                    name, filetype = filename.rsplit('.', 1)
-                else:
-                    name = filename
-                    filetype = None
                 if filetype != "py":
                     continue
-                module = imp.load_module(
-                    name,
-                    *imp.find_module(name, [dirpath])
-                )
-                raw_scarabs_classes = inspect.getmembers(
+                module = load_module(name, *find_module(name, [dirpath]))
+                raw_scarabs_classes = getmembers(
                     module,
-                    lambda member: (
-                        (
-                            hasattr(member, '__module__') and
-                            member.__module__ == name
-                        ) and
-                        inspect.isclass(member)
+                    lambda m: (
+                        (hasattr(m, '__module__') and m.__module__ == name) and
+                        isclass(m)
                     )
                 )
                 for scarab in raw_scarabs_classes:
-                    self.scarabs[scarab[0]] = scarab[1]()
+                    self._scarabs[scarab[0]] = scarab[1]()
 
     def load_scarabs(self):
         self.load_folder(self.config["default_scarabs_folder"])
         for folder in self.config["additional_scarabs_folders"]:
             self.load_folder(folder)
 
-    def get_scarabs(self):
-        return self.scarabs
-
-    def get_scarabs_names(self):
-        return set(self.scarabs.keys())
-
     def run_scarab(self, scarab_name):
         # TODO: Add another types of information storing (memory etc.)
         current_time = time.gmtime()
-        result_filename = "{}_{}-{}-{}_{}-{}-{}GM".format(
+        filename = "{}_{}-{}-{}_{}-{}-{}GM".format(
             scarab_name,
             current_time.tm_year,
             current_time.tm_mon,
@@ -76,12 +75,9 @@ class Overseer(object):
             current_time.tm_min,
             current_time.tm_sec,
         )
-        with codecs.open(
-                self.config["default_output_folder"] + '/' + result_filename,
-                mode='w',
-                encoding='utf-8'
-        ) as result_file:
-            for result in self.scarabs[scarab_name].run_default():
+        filepath = self.config["default_output_folder"] + '/' + filename
+        with codecs.open(filepath, mode='w', encoding='utf-8') as result_file:
+            for result in self._scarabs[scarab_name].run_default():
                 result_file.write(str(result))
 
 
